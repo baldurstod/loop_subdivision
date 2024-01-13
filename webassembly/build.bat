208beebe94d8f9bf958c050ff2afb4759f814611
Guillaume <Guillaume>
2024-01-13 11:49:25 +0100

    Changed build command line

--- webassembly/build.bat
+++ webassembly/build.bat
@@ -5,6 +5,9 @@ K:\Projects\WWW\emscripten\emsdk\upstream\emscripten\em++ ^
 -s ALLOW_MEMORY_GROWTH=1 ^
 -o loop_subdivision.wasm ^
 -s EXTRA_EXPORTED_RUNTIME_METHODS="['cwrap']" ^
--s WASM=1 -s ^
-ERROR_ON_UNDEFINED_SYMBOLS=0 ^
+-s WASM=1 ^
+-s ERROR_ON_UNDEFINED_SYMBOLS=0 ^
 wasm.cpp ../src/Mesh.cpp ../src/Edge.cpp ../src/LOOP.cpp ../src/FormTrait.cpp ../src/HalfEdge.cpp ../src/javascript.cpp ../src/Vertex.cpp
+
+rem -s INITIAL_MEMORY=67108864 ^
+rem -g2 ^
